2a8b2eaa628433f297350220841d9cce52ce8f6f
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-06-25 22:41:21 +0200

    firebase initialized outside of the component, getFirestore(app) initialized

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, ImageBackground } from "react-native";
 import Chat from "./components/Chat";
 import Start from "./components/Start";
@@ -6,25 +5,32 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth"; // Import Auth functions
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Stack = createNativeStackNavigator();
 
+const firebaseConfig = {
+  apiKey: "AIzaSyB3oiMb8lfHDfpRSjQvBp-8so_vsdlE7WM",
+  authDomain: "chat-app-c731f.firebaseapp.com",
+  projectId: "chat-app-c731f",
+  storageBucket: "chat-app-c731f.appspot.com",
+  messagingSenderId: "122301303822",
+  appId: "1:122301303822:web:2c3bd92678578fd6e1005f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+// Initialize Firebase Auth with persistence
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
 const App = () => {
-  const firebaseConfig = {
-    apiKey: "AIzaSyB3oiMb8lfHDfpRSjQvBp-8so_vsdlE7WM",
-    authDomain: "chat-app-c731f.firebaseapp.com",
-    projectId: "chat-app-c731f",
-    storageBucket: "chat-app-c731f.appspot.com",
-    messagingSenderId: "122301303822",
-    appId: "1:122301303822:web:2c3bd92678578fd6e1005f",
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-
-  // Initialize Cloud Firestore and get a reference to the service
-  const db = getFirestore(app);
-
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Start">
