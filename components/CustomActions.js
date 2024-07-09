76336913b3568adda4bd59af564838a3ca52c9ba
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-07-09 18:55:00 +0200

    customized button with "+" created

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,7 +1,37 @@
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity, StyleSheet, Text, View } from "react-native";
 
-const CustomActions = () => {
-  return <TouchableOpacity style={styles.container} />;
+const CustomActions = ({ wrapperStyle, iconTextStyle }) => {
+  const onActionPress = () => {};
+
+  return (
+    <TouchableOpacity style={styles.container} onPress={onActionPress}>
+      <View style={[styles.wrapper, wrapperStyle]}>
+        <Text style={[styles.iconText, iconTextStyle]}>+</Text>
+      </View>
+    </TouchableOpacity>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    width: 26,
+    height: 26,
+    marginLeft: 10,
+    marginBottom: 10,
+  },
+  wrapper: {
+    borderRadius: 13,
+    borderColor: "#b2b2b2",
+    borderWidth: 2,
+    flex: 1,
+  },
+  iconText: {
+    color: "#b2b2b2",
+    fontWeight: "bold",
+    fontSize: 10,
+    backgroundColor: "transparent",
+    textAlign: "center",
+  },
+});
 
 export default CustomActions;
